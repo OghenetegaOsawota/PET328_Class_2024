ad42aa99d4f979e6d53eeb1a547e047509aaaa0a
19CN026179 <105083965+OghenetegaOsawota@users.noreply.github.com>
2024-04-24 16:06:07 +0100

    Update block_classifier.py

--- block_classifier.py
+++ block_classifier.py
@@ -3,19 +3,22 @@
 # A script to classify gridblocks in a discretized
 # oil reservoir; based on the position of the gridblock
 # relative to the reservoir boundaries.
+def classify_block(i,j,nx,ny):
+    # Classify gridblock
+    if (i == 1 and j == 1) or (i == 1 and j == ny) or (i == nx and j == 1) or (i == nx and j == ny):
+        block_cat = 'IV'
+    elif j==1 or j==ny:
+         block_cat = 'II'
+    elif i==1 or i==nx:
+         block_cat = 'III'
+    else:
+         block_cat = 'I'
 
-# Receive inputs from user:
-i = int(input('What is the column index of the gridblock?'))
-j = int(input('What is the row index of the gridblock?'))
-nx = int(input('How may columns are there in the discerized reservoir?'))
-ny = int(input('How may rows are there in the discerized reservoir?'))
+    return (block_cat)
 
-# Classify gridblock
-if (i == 1 and j == 1) or (i == 1 and j == ny) or (i == nx and j == 1) or (i == nx and j == ny):
-    block_cat = 'IV'
-elif j==1 or j==ny:
-    block_cat = 'II'
-elif i==1 or i==nx:
-    block_cat = 'III'
-else:
-    block_cat = 'I'
+    # Receive inputs from user:
+    i = int(input('What is the column index of the gridblock?'))
+    j = int(input('What is the row index of the gridblock?'))
+    nx = int(input('How may columns are there in the discerized reservoir?'))
+    ny = int(input('How may rows are there in the discerized reservoir?'))
+
